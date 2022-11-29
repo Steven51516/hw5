754eb4c7747ae9274208354571f32747aa55260b
Sizhe Liu <sliu0727@usc.edu>
2022-11-29 07:32:10 +0000

    final submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,6 +19,23 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool valid(
+    AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::map<int, int>& shifts,
+    int row,
+    int col);
+
+bool solve_helper(
+    AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::map<int, int>& shifts,
+    int row,
+    int col);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -30,14 +47,69 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    if(avail.size() == 0){
         return false;
     }
     sched.clear();
     // Add your code below
+    std::map<int, int> shifts;
+    int days = avail.size();
+    int n_nurse = avail[0].size();
+		//shifts[i] means the total shifts for nurse i
+    for(int i = 0; i<n_nurse; i++) shifts[i]=0;
+		//initialize the sched matrix
+    for(int i = 0; i<days; i++)
+    {
+        std::vector<Worker_T> temp;
+        for(int j = 0; j<int(dailyNeed); j++) temp.push_back(0);
+        sched.push_back(temp);
+    }
+    AvailabilityMatrix A = avail;
+    return solve_helper(A, dailyNeed, maxShifts, sched, shifts, 0, 0);
+}
 
+bool solve_helper(
+    AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::map<int, int>& shifts,
+    int row,
+    int col)
+{
+    int n_nurse = avail[0].size();
+    for(int i = 0; i<int(n_nurse); i++)
+    {
+        sched[row][col]=i;
+  
+        if(valid(avail, dailyNeed, maxShifts, sched, shifts, row, col))
+        { 
+            shifts[i]+=1;
+            avail[row][i]=0;
+            if(row==int(avail.size()-1) && col==int(dailyNeed-1))  return true;
+            else if((col==int(dailyNeed-1))&&solve_helper(avail, dailyNeed, maxShifts, sched, shifts, row+1, 0)) return true;
+            else if(col!=int(dailyNeed-1)&&solve_helper(avail, dailyNeed, maxShifts, sched, shifts, row, col+1))  return true;
+            //backtrack if fails
+						shifts[i]-=1;
+            avail[row][i]=1;
+        }
+    }
+    return false;
+}
 
-
-
+//a helper function to determine whether the current sched matrix is valid
+bool valid(
+    AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::map<int, int>& shifts,
+    int row,
+    int col)
+{
+    int id = sched[row][col];
+    if(avail[row][id]==0) return false;
+    if(shifts[id]+1 > int(maxShifts)) return false;
+    return true;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -10,66 +10,11 @@
 using namespace std;
 
 
-// // Add prototypes of helper functions here
-// void helper(const std::string& in,
-//     std::vector<char>& floating,
-//     const std::set<std::string>& dict,
-// 		std::set<std::string>& answer, std::string word);
-
-// // Definition of primary wordle function
-// std::set<std::string> wordle(
-//     const std::string& in,
-//     const std::string& floating,
-//     const std::set<std::string>& dict)
-// {
-//     // Add your code here
-// 		std::set<std::string> answer;
-// 		std::vector<char> remain;
-// 		for(int i = 0; i<int(floating.size()); i++) remain.push_back(floating[i]);
-// 		helper(in, remain, dict, answer, "");
-// 		return answer;
-// }
-
-// // Define any helper functions here
-// void helper(const std::string& in,
-//     std::vector<char>& floating,
-//     const std::set<std::string>& dict,
-// 		std::set<std::string>& answer, std::string word)
-// {
-// 	int current = word.size();
-// 	if(current==int(in.size()))
-// 	{
-// 		if(dict.find(word)!=dict.end() && floating.size()==0) answer.insert(word);
-// 		return;
-// 	}
-// 	int num = 0;
-// 	for(int i = current; i<int(in.size()); i++)
-// 	{
-// 		if(in[i]=='-') num++;
-// 	}
-// 	if(num<int(floating.size())) return;
-// 	if(in[current]!='-')
-// 	{
-// 		helper(in, floating, dict, answer, word+in[current]);
-// 		return;
-// 	}
-// 	std::string atoz = "abcdefghijklmnopqrstuvwxyz";
-// 	for(int i = 0; i<int(floating.size()); i++)
-// 	{
-// 		char c = floating[i];
-// 		floating.erase(floating.begin()+i);
-// 		helper(in, floating, dict, answer, word+ c);
-// 		floating.insert(floating.begin()+i, c);
-// 		if(atoz.find(c)!=atoz.npos)	atoz.erase(atoz.begin()+atoz.find(c));
-// 	}
-// 	for(int i = 0; i<int(atoz.size()); i++) helper(in,floating, dict, answer, word+(atoz[i]));
-// }
-
 // Add prototypes of helper functions here
 void helper(const std::string& in,
-    std::multiset<char>& floating,
+    std::string& floating,
     const std::set<std::string>& dict,
-		std::set<std::string>& answer, std::string word);
+		std::set<std::string>& answer, std::string word, int num);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -79,43 +24,48 @@ std::set<std::string> wordle(
 {
     // Add your code here
 		std::set<std::string> answer;
-		std::multiset<char> remain;
-		for(int i = 0; i<int(floating.size()); i++) remain.insert(floating[i]);
-		helper(in, remain, dict, answer, "");
+		std::string remain = "";
+		for(int i = 0; i<int(floating.size()); i++) remain+=floating[i];
+		int num = 0;
+		//num is the number of the remaining unkown alphabets
+		for(int i = 0; i<int(in.size()); i++)
+		{
+			if(in[i]=='-') num+=1;
+		}
+		helper(in, remain, dict, answer, "", num);
 		return answer;
 }
 
 // Define any helper functions here
 void helper(const std::string& in,
-    std::multiset<char>& floating,
+    std::string& floating,
     const std::set<std::string>& dict,
-		std::set<std::string>& answer, std::string word)
+		std::set<std::string>& answer, std::string word, int num)
 {
 	int current = word.size();
 	if(current==int(in.size()))
 	{
-		if(dict.find(word)!=dict.end() && floating.size()==0) answer.insert(word); 
+		if(dict.find(word)!=dict.end() && floating.size()==0) answer.insert(word);
 		return;
 	}
 	if(in[current]!='-')
 	{
-		helper(in, floating, dict, answer, word+in[current]);	return;
+		helper(in, floating, dict, answer, word+in[current], num); return;
 	}
-  std::string atoz = "abcdefghijklmnopqrstuvwxyz";
-	for(std::multiset<char>::iterator it = floating.begin(); it!=floating.end(); ++it)
+	std::string atoz = "abcdefghijklmnopqrstuvwxyz";
+	for(auto it = floating.begin(); it!=floating.end(); ++it)
 	{
 		char c = *it;
 		floating.erase(it);
-		helper(in, floating, dict, answer, word+ c);
-		floating.insert(*it);
-		int a = atoz.find(*it);
-		if(a!=atoz.npos)	atoz.erase(atoz.begin()+a);
+		helper(in, floating, dict, answer, word+ c, num-1);
+		floating.insert(it, c);
+		int f = atoz.find(c);
+		//erase all floatings from atoz
+		if(f!=atoz.npos)	atoz.erase(atoz.begin()+f);
 	}
-	int num = 0;
-	for(int i = current+1; i<int(in.size()); i++)
-	{
-		if(in[i]=='-') num++;
-	}
-	if(num<int(floating.size())) return;
-	for(int i = 0; i<int(atoz.size()); i++) helper(in,floating, dict, answer, word+(atoz[i]));
+	//if the remaining places is equal to number of floatings, then only add floatings
+	if(num-1<int(floating.size())) return;
+	for(int i = 0; i<int(atoz.size()); i++) helper(in,floating, dict, answer, word+(atoz[i]), num-1);
 }
+
+
